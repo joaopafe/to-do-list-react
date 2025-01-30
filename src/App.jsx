4bde8d3092e264e04f891768a64e5b6c81a58e47
Joao Ferreira (SAO) <Joao.Ferreira2@fit-tecnologia.org.br>
2025-01-30 11:17:46 -0300

    add the components on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
+import TaskInput from "./components/TaskInput";
+import TaskList from "./components/TaskList";
+
 function App() {
-  return <></>;
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (newTask) => {
+    setTasks([
+      ...tasks,
+      {
+        id: Date.now(),
+        name: newTask,
+        done: false,
+      },
+    ]);
+  };
+
+  console.log(tasks);
+
+  return (
+    <>
+      <div className="page-title-section">
+        <h1 className="page-title">Lista de tarefas</h1>
+      </div>
+
+      <TaskInput onAddTask={addTask} />
+
+      <TaskList tasks={tasks} />
+    </>
+  );
 }
 
 export default App;
