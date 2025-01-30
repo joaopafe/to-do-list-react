8bf220cb28f6f4bd73ac28c3fd98ea7119e35e59
Joao Ferreira (SAO) <Joao.Ferreira2@fit-tecnologia.org.br>
2025-01-30 11:16:45 -0300

    add the TaskList component

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,13 @@
+import Task from "./Task";
+
+const TaskList = ({ tasks }) => {
+  return (
+    <div className="task-list">
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} />
+      ))}
+    </div>
+  );
+};
+
+export default TaskList;
